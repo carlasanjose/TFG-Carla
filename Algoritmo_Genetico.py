271912f7f07cddda6f64ef532e7c03a2680c1458
carlasanjose <98812058+carlasanjose@users.noreply.github.com>
2023-07-01 15:08:16 +0200

    Update Algoritmo_Genetico.py

--- Algoritmo_Genetico.py
+++ Algoritmo_Genetico.py
@@ -1,30 +1,26 @@
 import pandas as pd
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 #Parámetros del problema
 num_iteraciones=500             #número de iteraciones permitidas
 n=20                            #tamaño de la población
 pc=0.2                          #porcentaje de cruzamiento
-ub=1000                         #límite superior
-lb=-(1000)                      #límite inferior
+ub=1000                          #límite superior
+lb=-(1000)                       #límite inferior
 nc=6                            #número de "parejas" descendientes 
 nm=round(0.3*n)                 #número de individuos mutados
 mu=0.1                          #tasa de mutación
 epsilon=0.00001                 #condición de convergencia
-soluciones =[]                  #En esta lista almacenamos las 3 soluciones de la ecuación cúbica 
-
 
 # Ecuación:     x^3 + 2x^2 - 5x - 6 = 0
 # Soluciones:   x1 = -3 ; x2 = -1 ; x3 = 2
 
-a = 1
-b = 0
-c = -(2)
-d = -(5)
 
-def fitness(df,a,b,c,d):
-    fitness = abs(a*df['Valor']**3+b*df['Valor']**2+c*df['Valor']+d) #Función objetivo del problema 
+
+def fitness(df):
+    fitness = abs(df['Valor']**3-2*df['Valor']-5) 
     return fitness
 
 #CREACIÓN POBLACIÓN
@@ -33,22 +29,22 @@ def creacion_poblacion():
     pob['Individuo']=range(0, n)  #Añado una columna con el número de cada individuo
     pob['Valor']=np.random.uniform(lb, ub+1, size=len(pob)) # Columna valor generada en el DataFrame pob con valores aleatorios distribuidos en el espacio de búsqueda                                                       #uniformemente en el rango [lb, ub] para cada posición i 
     #Evaluación en la función objetivo:
-    pob['Fitness']= fitness(pob,a,b,c,d) #Necesita que se haya definido la función fitness
+    pob['Fitness']= fitness(pob) #Necesita que se haya definido la función fitness
     return pob
 
 # SELECCIÓN--------------
 def seleccion(P):
     r1=np.random.random()
     r2=np.random.random()      
-    P_sel=P.copy()
-    P_sel['Mayor_r1']=np.where((P['Fitness_Norm_Acum']>r1),1,0) 
-    P_sel['Mayor_r2']=np.where((P['Fitness_Norm_Acum']>r2),1,0) 
-    min_umbral_r1=P_sel[P_sel['Mayor_r1']==P_sel['Mayor_r1'].max()]['Fitness_Norm_Acum'].min() # Con max(), nos quedamos con todos los que tienen un 1 en esa columna 
+    P_ruleta=P.copy()
+    P_ruleta['Mayor_r1']=np.where((P['Prob_Acum']>r1),1,0) 
+    P_ruleta['Mayor_r2']=np.where((P['Prob_Acum']>r2),1,0) 
+    min_mayor_r1=P_ruleta[P_ruleta['Mayor_r1']==P_ruleta['Mayor_r1'].max()]['Prob_Acum'].min() # Con max(), nos quedamos con todos los que tienen un 1 en esa columna 
                                                                                                # y con el .min() hacemos que sea el primero que sobrepasa el r1 (primero que cumple ser mayor que r1 en prob acum es el umbral)
                                                                                                                                                                                     
-    padre_1=P_sel[P_sel['Fitness_Norm_Acum']==min_umbral_r1]
-    min_umbral_r2=P_sel[P_sel['Mayor_r2']==P_sel['Mayor_r2'].max()]['Fitness_Norm_Acum'].min()    
-    padre_2=P_sel[P_sel['Fitness_Norm_Acum']==min_umbral_r2]
+    padre_1=P_ruleta[P_ruleta['Prob_Acum']==min_mayor_r1]
+    min_mayor_r2=P_ruleta[P_ruleta['Mayor_r2']==P_ruleta['Mayor_r2'].max()]['Prob_Acum'].min()    
+    padre_2=P_ruleta[P_ruleta['Prob_Acum']==min_mayor_r2]
     return padre_1, padre_2
 
 # CRUCE--------------------
@@ -87,8 +83,9 @@ def limites(df, columna, lb, ub):
 
 
 ##########CREACIÓN POBLACIÓN###########################################################
-
-def algoritmo_genetico(a,b,c,d):
+errores=[]
+iteraciones=[]
+def algoritmo_genetico():
     pob=creacion_poblacion()
     xMejor = pob[pob['Fitness'] == pob['Fitness'].min()] #Es un dataframe con una fila (mejor)
     resumen_iteraciones=pd.DataFrame(columns=['Numero_iteracion', 'Mejor_Fitness','Mejor_Valor'])        
@@ -96,8 +93,8 @@ def algoritmo_genetico(a,b,c,d):
     aux_indice=0 # auxiliar para actualizar el índice de los individuos
     while xMejor['Fitness'].iloc[0] > epsilon and i < num_iteraciones :       
             P=pob.copy() 
-            P['Fitness_Norm']=P['Fitness']/P['Fitness'].sum() #Añado la columna Aptitud normalizada
-            P['Fitness_Norm_Acum']=np.cumsum(P['Fitness_Norm']) #Añado la columna suma acumulada de la aptitud normalizada
+            P['Probabilidad']=P['Fitness']/P['Fitness'].sum() #Añado la columna Aptitud normalizada
+            P['Prob_Acum']=np.cumsum(P['Probabilidad']) #Añado la columna suma acumulada de la aptitud normalizada
             popc=pd.DataFrame(columns=pob.columns)  #las columnas son individuo, posiciones y fitness ES UN DATAFRAME con esas tres columnas
             for j in range(0,nc): #Dos hijos descendientes
                 ##########SELECCIÓN: Para elegir a los padres###############################################
@@ -107,10 +104,10 @@ def algoritmo_genetico(a,b,c,d):
                 n_individuo_hijo_1 = int(n + (aux_indice * 2)) 
                 n_individuo_hijo_2 = int(n + (aux_indice * 2) + 1)     
                 popc_it=pd.DataFrame(columns=pob.columns) #almacena a cada par de hijos
-                popc_it.loc[j]=[n_individuo_hijo_1] + hijo_1 + [np.nan]
-                popc_it.loc[j+2]= [n_individuo_hijo_2] + hijo_2 + [np.nan]    
+                popc_it.loc[len(popc)]=[n_individuo_hijo_1] + hijo_1 + [np.nan]
+                popc_it.loc[len(popc)+1]= [n_individuo_hijo_2] + hijo_2 + [np.nan]    
                 limites(popc_it, 'Valor', lb, ub) #recorre los valores de los nuevos hijos y ajusta segun los límites    
-                popc_it['Fitness']= fitness(popc_it,a,b,c,d)  #calcula fitness para cada nuevo hijo 
+                popc_it['Fitness']= fitness(popc_it)  #calcula fitness para cada nuevo hijo 
                 popc=pd.concat([popc, popc_it], axis=0) 
                 aux_indice+=1 
             ######################ETAPA DE MUTACIÓN#####################################
@@ -128,7 +125,7 @@ def algoritmo_genetico(a,b,c,d):
                 mutacion(xa) 
                 popm.loc[k, 'Valor']=xa.iloc[0]['Valor'] 
                 limites(popm, 'Valor', lb, ub)      
-                popm['Fitness']= fitness(popm,a,b,c,d) # Relleno la columna Fitness
+                popm['Fitness']= fitness(popm) # Relleno la columna Fitness
                     
         ########FIN ETAPA DE MUTACION################################################
         ##ETAPA DE RECOMINACIÓN: Elitismo##
@@ -140,39 +137,27 @@ def algoritmo_genetico(a,b,c,d):
             #print('Iteración:' , i)
             resumen_iteraciones.loc[i] = [i, xMejor['Fitness'].values[0], xMejor['Valor'].values[0]]
 
-            i+=1    
+            i+=1
+            mej_fit=xMejor['Fitness'].values[0]  
+            # errores.append(mej_fit)
+            # iteraciones.append(i) 
+        
     sol=xMejor['Valor'].values[0]
-    soluciones.append(sol)
-   
-    # if len(soluciones) ==1:
-    #     r = 0
-    #     s = a
-    #     t = b + s * sol
-    #     u = c + t * sol 
-    #     print(i)
-    #     algoritmo_genetico(r,s,t,u)        
-
-    # elif len(soluciones)==2: 
-    #     x2=soluciones[1]
-    #     x3=-(c/b)-(x2)
-    #     print(i)
-    #     soluciones.append(x3)
-  
-    # else: 
-    #     pass   
-
-    return soluciones,  i, resumen_iteraciones
+    mej_fit=xMejor['Fitness'].values[0]   
+    # errores.append(mej_fit)
+    # iteraciones.append(i) 
+    return sol,  i, resumen_iteraciones, mej_fit
 
 # Registro del tiempo de inicio
 inicio = time.time()
 
-print(pc,n, nc)
-soluciones,i,iter = algoritmo_genetico(a,b,c,d)        
-print("x1=", soluciones[0])#, ";x2=", soluciones[1], ";x3=", soluciones[2])
-print("Iteracion:",i)
-print(iter)
-
-# Registro del tiempo de finalización
+solucion,i,iter,mej_fit = algoritmo_genetico()        
+print("Solución obtenida por el GA: ", solucion) 
+print("Error cometido: ", mej_fit)
+print("Iteración: ",i)
+# print(iter)
+# print(iter['Mejor_Fitness'].iloc[i-1])
+# Registro del tiempo de fisnalización
 fin = time.time()
 
 # Cálculo del tiempo transcurrido
@@ -180,5 +165,21 @@ tiempo_transcurrido = fin - inicio
 
 # Imprimir el tiempo transcurrido en segundos
 print("Tiempo transcurrido:", tiempo_transcurrido, "segundos")
+
+
+# # Listas de errores y iteraciones
+
+# # Crear gráfica
+# plt.plot(iteraciones, errores, marker='o')
+
+# # Configurar etiquetas de los ejes
+# plt.xlabel('Iteraciones')
+# plt.ylabel('Error')
+
+# # Configurar título del gráfico
+# plt.title('Gráfica de Error vs. Iteraciones')
+
+# # Mostrar la gráfica
+# plt.show()
 
 
